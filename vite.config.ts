8bd750f312c0305cd58eedc05aec2979aed7eeec
Oktz <oktznih@outlook.com>
2026-06-19 20:45:01 +0700

    Configure Vite server settings for development

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ dotenv.config({ path: '.env.local' });
 dotenv.config({ path: '.env' });
 dotenv.config();
 
+server: {
+    allowedHosts: 'all',
+    host: true,
+    port: 5173,
+  }
+})
+
 export default defineConfig((config) => {
   return {
     define: {
